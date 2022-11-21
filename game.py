f1be06b1dac62ef853a2300f33c3811c15bf997d
Claire Kelley <clairekel23@gmail.com>
2022-11-21 12:24:06 -0500

    Created modules for RPSLS

---
+++ game.py
@@ -0,0 +1,11 @@
+
+class Game:
+
+    def __init__(self):
+        pass
+
+    def display_welcome(self):
+        self.display_welcome = "Welcome to Rock Paper Scissors Lizard Spock. First player to win two agames is the WINNER!"
+    
+    def game_rules(self):
+        print.self.game_rules = "Scissors cut paper\nPaper covers Rock\nRock crushes Lizard\nLizard poisons Spock\nSpock smashes scissors\nScissors decapitates Lizard\nLizard eats paper\nPaper disproves Spock\nSpock vaporizes Rock\nRock Crushes scissors"
